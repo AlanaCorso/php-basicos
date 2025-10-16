95b82f5607ce4994655c66a6013951dca8a1f501
Alana <alana.corso@aluno.senai.br>
2025-10-16 08:32:39 -0300

    php
    introdução

---
+++ Aula1/exemplo1.php
@@ -0,0 +1,14 @@
+<?php
+    // Geração de texto (string); string = texto
+    echo "Chama Sesi-Senai!";
+
+    /*
+    Geração com texto estruturado
+    Com tags e atributos
+    */
+    // Aspas simples dentro das aspas dupla
+    echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP</h2>";
+
+    // Aspas dupla dentro de aspas dupla - barra antes \"  \"
+    echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP</h2>";
+?>
